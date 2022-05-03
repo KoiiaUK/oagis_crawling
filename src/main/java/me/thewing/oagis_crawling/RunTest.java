80b7a9c076baeef755324b59abfd04291cc9d893
sujl95 <sujl95@naver.com>
2022-05-04 00:07:02 +0900

    nouns

--- src/main/java/me/thewing/oagis_crawling/RunTest.java
+++ src/main/java/me/thewing/oagis_crawling/RunTest.java
@@ -1,9 +1,24 @@
 package me.thewing.oagis_crawling;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class RunTest {
 
   public static void main(String[] args) {
+    Document doc = null;
 
+    String url = "file:///C:/Users/SungJun/Desktop/OAGIS_10_6_EnterpriseEdition/OAGi-BPI-Platform/org_openapplications_oagis/10_6/Documentation/OAGIS.html";
+
+    try {
+      doc = Jsoup.connect(url).get();
+
+      System.out.println("doc = " + doc);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
