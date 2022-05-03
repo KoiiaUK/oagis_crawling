ee68f1c3dda2af762f96ac24ab6544c8f8ae651f
sujl95 <sujl95@naver.com>
2022-05-03 23:46:28 +0900

    init

---
+++ src/main/java/me/thewing/oagis_crawling/RunTest.java
@@ -0,0 +1,9 @@
+package me.thewing.oagis_crawling;
+
+public class RunTest {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/test/java/me/thewing/oagis_crawling/OagisCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.thewing.oagis_crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OagisCrawlingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
